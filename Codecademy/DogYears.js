713ef148cb29b30918cb07a78cc95abfd1c2811d
greenDNA <greenDNA@users.noreply.github.com>
2018-11-15 18:13:41 -0500

    Create DogYears.js

---
+++ Codecademy/DogYears.js
@@ -0,0 +1,9 @@
+var myAge = 23; //my current age
+var earlyYears = 2; //value of variable will change
+earlyYears *= 10.5;
+var laterYears = myAge - 2; //difference of myAge by 2
+laterYears*=4; //number of dog years accounted for by later years
+console.log(`${earlyYears} early years. ${laterYears} later years.`);
+var myAgeInDogYears = earlyYears + laterYears; //value of age in dog years
+var myName = "trayvont".toLowerCase(); //converts string to all lowercase and store it as myName
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); //prints to console variable values with strings
